ff38720af2cc5a88d24e9a52a88f0ab84139fb41
johncovenant <ufotjohnamos@gmail.com>
2023-04-18 20:53:39 +0100

    using CSS modules instead

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,41 +1,45 @@
-import styled from 'styled-components';
-// import './Button.css';
+import React from "react";
 
-const Button = styled.button`
-  font: inherit;
-  width: 100%;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: green;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// import styled from 'styled-components';
+import styles from './Button.module.css';
 
-  @media (min-width: 768px){
-    width: auto;
-  }
+//COMMENTED OUT STYLED COMPONENTS TO USE CSS MODULES. GRACIAS
 
-&:focus {
-  outline: none;
-}
+// const Button = styled.button`
+//   font: inherit;
+//   width: 100%;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: green;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-&:hover,
-&:active {
-  background: #ac0e77;
-  border-color: #ac0e77;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
+//   @media (min-width: 768px){
+//     width: auto;
+//   }
 
-`;
+// &:focus {
+//   outline: none;
+// }
+
+// &:hover,
+// &:active {
+//   background: #ac0e77;
+//   border-color: #ac0e77;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }
+
+// `;
 
 //TESTING STYLED COMPONENTS INSTEAD
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
