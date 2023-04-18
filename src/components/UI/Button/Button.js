f0e40d5576465f00629dc4c4c24306d669febd9d
johncovenant <ufotjohnamos@gmail.com>
2023-04-18 20:57:19 +0100

    cssmodules implemented

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -36,7 +36,7 @@ import styles from './Button.module.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
