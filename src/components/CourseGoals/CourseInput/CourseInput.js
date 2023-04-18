8d5e74870d0fbdd14da7b82bcd7d670d8c63dbee
johncovenant <ufotjohnamos@gmail.com>
2023-04-18 20:36:55 +0100

    media queries

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -10,12 +10,14 @@ const FormControl = styled.div`
   font-weight: bold;
   display: block;
   margin-bottom: 0.5rem;
+  color: ${props => (props.invalid ? 'red' : 'black')};
 }
 
 & input {
   display: block;
   width: 100%;
   border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+  background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
   font: inherit;
   line-height: 1.5rem;
   padding: 0 0.25rem;
@@ -27,14 +29,6 @@ const FormControl = styled.div`
   border-color: #8b005d;
 }
 
-&.invalid input {
-  border-color: red;
-  background: #ffd7d7;
-}
-
-&.invalid label {
-  color: red;
-}
 `;
 
 const CourseInput = props => {
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,12 +3,17 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   font: inherit;
+  width: 100%;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
   color: white;
   background: green;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px){
+    width: auto;
+  }
 
 &:focus {
   outline: none;
