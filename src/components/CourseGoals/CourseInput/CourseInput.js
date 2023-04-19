1bae024f3369ab8b8d3c5a48ddfaff25203f5830
johncovenant <ufotjohnamos@gmail.com>
2023-04-19 20:53:43 +0100

    yesterday's change

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,36 +1,37 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 // {using styled components instead} import './CourseInput.css';
+// okay okay changing to css modules now. last chance. let me comment my styled components.
 import styles from './CourseInput.module.css';
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => (props.invalid ? 'red' : 'black')};
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => (props.invalid ? 'red' : 'black')};
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-  background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//   background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
 
-`;
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -54,17 +55,18 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      {//USING STYLED COMPONENTS INSTEAD. HENCE, FORMCONTROL INSTEAD OF DIV
+      {//USING STYLED COMPONENTS INSTEAD. HENCE, FORMCONTROL INSTEAD OF DIV.
+        // sorry changing to CSS modules. CJISLOST
 
-        <FormControl invalid={!isValid}>
+        // <FormControl invalid={!isValid}>
+        //   <label >Goals</label>
+        //   <input type="text" onChange={goalInputChangeHandler} />
+        // </FormControl>
+
+        <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
           <label >Goals</label>
           <input type="text" onChange={goalInputChangeHandler} />
-        </FormControl>
-
-     /* <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
-        <label >Goals</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div> */}
+        </div>}
       <Button type="submit">Add Goal</Button>
     </form>
   );
